0625e513bc9cc5ba4633b5785dcc14e7b7a77449
津坂裕己 <hirokitsusaka@dhcp-163-178.iis.u-tokyo.ac.jp>
2025-05-16 21:47:33 +0900

    scripts changed

--- scripts/for_rovibrational_excitation/check_core_electric_field.py
+++ scripts/for_rovibrational_excitation/check_core_electric_field.py
@@ -1,7 +1,7 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../src")))
-from src.rovibrational_excitation.core.electric_field import ElectricField, gaussian_fwhm
+from rovibrational_excitation.core.electric_field import ElectricField, gaussian_fwhm
 import numpy as np
 
 duration = 20
